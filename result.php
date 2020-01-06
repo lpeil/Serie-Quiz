099310ce2b93be269f1497c5de84e632cf17267c
Luan Peil <luan@peil.dev>
2020-01-06 20:49:19 -0300

    Result of the test

--- result.php
+++ result.php
@@ -1,2 +1,36 @@
-<? 
-var_dump($_POST);
+<? require_once('data/questions.php');
+
+$sum = [
+    0 => 0,
+    1 => 0,
+    2 => 0,
+    3 => 0,
+    4 => 0
+];
+
+for($i = 0; $i < count($_POST); $i++) {
+    $sum[$_POST[$i]]++;
+}
+
+$mostSelected = array_keys($sum, max($sum));
+$qtyMostSelected = count($mostSelected);
+
+if($qtyMostSelected > 1) {
+    for($i = 0; $i < $qtyMostSelected; $i++) {
+        if($_POST[4] == $mostSelected[$i]) {
+            $mostSelected = $mostSelected[$i];
+        } elseif($_POST[3] == $mostSelected[$i]) {
+            $selectAux = $mostSelected[$i];
+        }
+    }
+    if($mostSelected != $_POST[4]) {
+        $mostSelected = $selectAux;
+    }
+} else {
+    $mostSelected = $mostSelected[0];
+}
+
+echo '<div>
+    <h1>'.$result[$mostSelected]["title"].'</h1>
+    <h3>'.$result[$mostSelected]["text"].'</h3>
+</div>';
