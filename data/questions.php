00b8cb537a428cf272b7c059ec076fa78ff74858
Luan Peil <luan@peil.dev>
2020-01-06 18:25:32 -0300

    Start the project

---
+++ data/questions.php
@@ -0,0 +1,73 @@
+<?
+$questions = [
+    "De manhã, você:",
+    "Indo para o trabalho você encontra uma senhora idosa caída na rua.",
+    "Chega no prédio e o elevador está cheio.",
+    "Você chega no trabalho e as convenções sociais te obrigam a puxar assunto.",
+    "A pauta pegou o dia todo, mas você está indo para casa."
+];
+
+$answers = array(
+    [
+        "Acorda cedo e come frutas cortadas metodicamente.",
+        "Sai da cama com o despertador e se prepara para a batalha da semana.",
+        "Só consegue lembrar do seu nome depois do café",
+        "Levanta e faz café pra todos da casa.",
+        "Passa o café e conserta um erro no HTML."
+    ],
+    [
+        "Ela vai atrapalhar seu horário. Oculte o corpo.",
+        "Levanta a senhora e jura protegê-la com sua vida.",
+        "Ajuda-a, mas questiona sua real identidade.",
+        "Oferece para caminharem juntos até um destino em comum.",
+        "Testa se ela roda bem no Firefox. Não roda."
+    ],
+    
+    [
+        "Convence parte das pessoas a esperarem o próximo.",
+        "Ignora as pessoas no elevador e entra de qualquer forma.",
+        "Você questiona a realidade, as coisas e tudo mais. Sobe de escada.",
+        "Com uma leve intimidação passivo-agressiva, encontra um lugar no elevador.",
+        "Cria um app que mostra a lotação do elevador. Vende o app e fica milionário."
+    ],
+    
+    [
+        "Fala sobre a política, eleições, como tudo é um absurdo.",
+        "Larga uma frase polêmica e vê uma pequena guerra se formar.",
+        "Puxa um assunto e te lembram que já foi discutido semana passada.",
+        "Sugere que os colegas trabalhem na ideia de um novo projeto.",
+        "Desabafa sobre como odeia PHP. Todo mundo na sala adora PHP."
+    ],
+    
+    [
+        "Vou chamar aqui o meu Uber.",
+        "Pegarei o bus junto com o resto do povo.",
+        "No ponto de ônibus mais uma vez, espero não errar a linha de novo.",
+        "Vou de carro, mas ofereço uma carona para os colegas.",
+        "TAcho que descobri uma forma de fazer aquela senhora rodar no Firefox."
+    ],
+    
+);
+
+$result = array(
+    [
+        "title" => "House of Cards",
+        "text" => "Você é House of Cards: ataca o problema com método e faz de tudo para resolver a situação."
+    ],
+    [
+        "title" => "Game of Thrones",
+        "text" => "Você é Game of Thrones: não tem muita delicadeza nas ações, mas resolve o problema de forma prática."
+    ],
+    [
+        "title" => "Lost",
+        "text" => "Você é Lost: faz as coisas sem ter total certeza se é o caminho certo ou se faz sentido, mas no final dá tudo certo."
+    ],
+    [
+        "title" => "Breaking Bad",
+        "text" => "Você é Breaking Bad: pra fazer acontecer você toma a liderança, mas sempre contando com seus parceiros."
+    ],
+    [
+        "title" => "Silicon Valley",
+        "text" => "Você é Silicon Valley: vive a tecnologia o tempo todo e faz disso um mantra para cada situação no dia."
+    ],
+)
---
+++ index.php
@@ -0,0 +1,23 @@
+<? 
+    require_once('data/questions.php');
+
+    var_dump($answers);
+?>
+
+<html>
+    <head>
+        <title>Sua Série</title>
+    </head>
+    <body>
+        <form class="question">
+            <div class="title">De manhã, você</div>
+            <div class="answers">
+                <label class="answer">
+                    <input type="radio" value="" name="aswer" />
+                    <span class="aswertText">
+                    </span>
+                </label>
+            </div>
+        </form>
+    </body>
+</html>
